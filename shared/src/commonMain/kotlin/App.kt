750fcfa69aae0c445f862d37a3d24bd681642e5e
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2023-03-28 20:32:55 +0900

    Update

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,3 +1,6 @@
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -6,16 +9,26 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 internal fun App() {
     MaterialTheme {
-        var text by remember { mutableStateOf("Hello, World!") }
-
-        Button(onClick = {
-            text = "Hello, ${getPlatformName()}"
-        }) {
-            Text(text)
+        var count by remember { mutableStateOf(0) }
+        Box(modifier = Modifier.fillMaxSize()) {
+            Column(modifier = Modifier.align(Alignment.Center)) {
+                Text("$count")
+                Button(onClick = { count += 1 }) {
+                    Text("PLUS")
+                }
+                Button(onClick = { count -= 1 }) {
+                    Text("MINUS")
+                }
+                Button(onClick = { count = 0 }) {
+                    Text("RESET")
+                }
+            }
         }
     }
 }
